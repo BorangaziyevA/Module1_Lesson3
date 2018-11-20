8b5a7ea6a18c9c1307b07df5b38f2b06cabb8bb3
Алишер Борангазиев <@local.itstep>
2018-11-20 21:29:58 +0600

    Добавьте файлы проекта.

---
+++ Project6/Source.cpp
@@ -0,0 +1,152 @@
+#include<stdio.h>
+#include<iostream>
+#include<locale.h>
+
+#define MaxLength 123
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+	//int a, b, c;
+	//scanf_s("%d", &a);
+	//scanf_s("%d", &b);
+	//scanf_s("%d", &c);
+
+	////b = > a;	a = > c;	c = > b	;
+
+	//int q = b;
+	//int w = c;
+	//c = q;
+	//b = a;
+	//a = w;
+	//
+	//printf("%d\n", a);
+	//printf("%d\n", b);
+	//printf("%d\n", c);
+
+	//int a;
+	//printf("cm = ");
+	//scanf_s("%d", &a);
+	//int b = a / 100;
+	//printf("m = ");
+	//printf("%d\n", b);
+	//
+	//
+
+	//int a, b, c;
+	//scanf_s("%d", &a);
+	//scanf_s("%d", &b);
+	//scanf_s("%d", &c);
+
+	////b = > c;	a = > b;	c = > a;
+
+	//int q = b;
+	//int w = c;
+	//c = a;
+	//b = w;
+	//a = q;
+	//
+	//printf("%d\n", a);
+	//printf("%d\n", b);
+	//printf("%d\n", c);
+
+	//int x, y;
+	//printf("x = ");
+	//scanf_s("%d", &x);
+
+	//printf("y = ");
+	//scanf_s("%d", &y);
+
+	//int z = x;
+	//x = y;
+	//y = z;
+
+	//printf("%d\n", x);
+	//printf("%d\n", y);
+
+	//float x, y;
+	//printf("Население = ");
+	//scanf_s("%f", &x);
+	//printf("Площадь = ");
+	//scanf_s("%f", &y);
+	//float z = x / y;
+	//printf("Плотность населения = ");
+	//printf("%f\n", z);
+
+	//float x, y;
+	//printf("Объем = ");
+	//scanf_s("%f", &x);
+	//printf("Масса тела = ");
+	//scanf_s("%f", &y);
+	//float z = x / y;
+	//printf("Плотность = ");
+	//printf("%f\n", z);
+
+
+	//float a, b;
+	//printf("a = ");
+	//scanf_s("%d", &a);
+	//printf("b = ");
+	//scanf_s("%d", &b);
+	//float x = 3.56*a+b*b*b-5.8*b*b+3.8*a-1.5;
+	//printf("%f\n", x);
+
+	//float x, y;
+	//printf("x = ");
+	//scanf_s("%d", &x);
+	//printf("y = ");
+	//scanf_s("%d", &y);
+	//float z = x * x*x - 2.5*x*y + 1.78*x*x - 2.5*y + 1;
+	//printf("%f\n", z);
+
+	//int x;
+	//printf("Длина ребра куба = ");
+	//scanf_s("%d", &x);
+	//int y = x * x*x;
+	//printf("Объём куба = ");
+	//printf("%d\n", y);
+	//int z = x * x;
+	//printf("Площадь боковой стороны = ");
+	//printf("%d\n", z);
+
+	//int x = 6350;
+	//int y = x * 2;
+	//printf("%d\n", y);
+
+	//int x;
+	//printf("Радиус = ");
+	//scanf_s("%d", &x);
+	//int y = x + x;
+	//printf("Диаметр = ");
+	//printf("%d\n", y);
+
+	//int x;
+	//printf("Сторона квадрата =");
+	//scanf_s("%d", &x);
+	//int y = 4 * x;
+	//printf("%d\n", y);
+
+	//int x;
+	//scanf_s("%d", &x);
+	//int y = 12 * x*x + 7 * x - 16;
+	//printf("%d\n", y);
+
+	//int x;
+	//scanf_s("%d", &x);
+	//int y = (7 * x*x) - 3 * x + 6;
+	//printf("%d\n",y);
+
+	//printf("x 25\n");
+	//printf("x y\n");
+
+
+	//printf("100 t\n");
+	//printf("1949 v\n");
+
+	//printf("5 ");
+	//printf("10\n");
+	//printf("7см\n");
+
+	system("pause");
+}
